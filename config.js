a6d9fc739e1eef4aa5c4d10aecb5b8fe1c4f65ee
Spatonn <94061852+Spatonn@users.noreply.github.com>
2023-11-27 18:10:26 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://simple-proxy.shivpatel2051.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YzlhZTYyNzk2NDY4MDc1YTViMThhOGRhOTBlZjdlYiIsInN1YiI6IjY1NjUxOWM3MjQ0MTgyMDEwY2I2MDkxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EB0rdmufVjuHT5iQ9HnN5SKCATcpP__ksL1dQrn8-A4"
+};
